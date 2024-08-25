4872b8ba8675b7a4c2df900b43c6fbc3497213c8
Zachary Blasczyk <zachary.blasczyk@wandb.com>
2024-08-24 22:19:58 -0500

    fix: Some TF best practices and naming

--- example/main.tf
+++ example/main.tf
@@ -1,13 +1,13 @@
 provider "google" {
   project = var.project_id
   region  = var.region
-  zone    = var.zone
+  # zone    = var.zone
 }
 
 provider "google-beta" {
   project = var.project_id
   region  = var.region
-  zone    = var.zone
+  # zone    = var.zone
 }
 
 module "ctrlplane" {
