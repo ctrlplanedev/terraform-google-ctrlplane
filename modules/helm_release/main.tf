6efc0020b429249594819b8fdc5e61cc013d38de
Justin Brooks <jsbroks@gmail.com>
2024-10-18 21:42:18 -0400

    fix: clean up passage of version tags

--- modules/helm_release/main.tf
+++ modules/helm_release/main.tf
@@ -1,22 +1,27 @@
 locals {
 
   config = {
-    global = { fqdn = var.fqdn }
-  }
+    global = {
+      fqdn = var.fqdn,
+      postgresql = {
+        user     = var.postgres_user,
+        password = var.postgres_password,
+        host     = var.postgres_host,
+        port     = tostring(var.postgres_port),
+        database = var.postgres_database,
+      },
 
-  image_tags = {
-    "webservice.image.tag"         = "f505293",
-    "migrations.image.tag"         = "a6faaf3",
-    "event-worker.image.tag"       = "f505293",
-    "job-policy-checker.image.tag" = "f505293",
-  }
+      redis = {
+        host     = var.redis_host,
+        password = var.redis_password,
+        port     = tostring(var.redis_port),
+      },
+    },
 
-  postgres_settings = {
-    "global.postgresql.user"     = var.postgres_user,
-    "global.postgresql.password" = var.postgres_password,
-    "global.postgresql.host"     = var.postgres_host,
-    "global.postgresql.port"     = tostring(var.postgres_port),
-    "global.postgresql.database" = var.postgres_database,
+    webservice           = { image = { tag = "f505293" } },
+    migrations           = { image = { tag = "a6faaf3" } },
+    "event-worker"       = { image = { tag = "f505293" } },
+    "job-policy-checker" = { image = { tag = "f505293" } },
   }
 
   integrations_settings = var.github_bot != null ? {
@@ -31,12 +36,6 @@ locals {
   auth_providers_settings = {
     "global.authProviders.google.clientId"     = var.google_auth.client_id,
     "global.authProviders.google.clientSecret" = var.google_auth.client_secret,
-  }
-
-  redis_settings = {
-    "global.redis.host"     = var.redis_host,
-    "global.redis.password" = var.redis_password,
-    "global.redis.port"     = tostring(var.redis_port),
   }
 
   ingress_annotations = {
@@ -57,12 +56,6 @@ locals {
   }
 }
 
-locals {
-  default_values = {}
-  # Merge default values with overrides
-  merged_values = merge(local.default_values, var.values)
-}
-
 resource "helm_release" "this" {
   name       = "ctrlplane"
   chart      = "ctrlplane"
@@ -71,19 +64,17 @@ resource "helm_release" "this" {
 
   dynamic "set" {
     for_each = merge(
-      local.image_tags,
       local.auth_providers_settings,
-      local.postgres_settings,
-      local.redis_settings,
       local.ingress_annotations,
       local.service_account_annotations,
       local.integrations_settings,
     )
+
     content {
       name  = set.key
       value = set.value
     }
   }
 
-  values = [yamlencode(local.merged_values), yamlencode(local.config)]
+  values = [yamlencode(merge(local.config, var.values))]
 }
