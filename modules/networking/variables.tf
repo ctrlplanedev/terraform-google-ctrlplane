e082fdf36aab370d55b1168487dd1bd7da6e034f
Zachary Blasczyk <zachary.blasczyk@wandb.com>
2024-12-09 08:58:41 -0600

    fix nat

--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -2,3 +2,8 @@ variable "namespace" {
   type        = string
   description = "The name prefix for all resources created."
 }
+
+variable "region" {
+  type        = string
+  description = "The region where the router and NAT gateway will be created"
+}
